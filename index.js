1c3d18e1e6b7caea9217ee55f8f90f59668b08af
akash dhiman <akash@ezimaxtechnologies.com>
2023-05-24 17:31:41 +0530

    user model and routing file chnaged

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const server = express();
 const router = require('./Routes/products')
+const userRouter = require('./Routes/users')
 
 // It Will To Parse The Body Data.
 server.use(express.json());
@@ -10,7 +11,7 @@ server.use(express.static('public'));
 
 // Router Configuration As A Middleware.
 server.use('/products/v1', router.routes.productRouter);
-server.use('/users/v1', router.routes.productRouter);
+server.use('/users/v1', userRouter.routes.userRouter);
 
 server.listen(8080, () => {
     console.log("Listen");
